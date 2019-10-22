0ea91ebc19be0b7a9d491b0494326dba0dccf435
HMHamster <hmhamster88@gmail.com>
2019-10-22 23:17:46 +0300

    settings added

--- index.js
+++ index.js
@@ -27,6 +27,55 @@ const tileTypes = {
   desert: {
     style: 'desert',
     text: 'D'
+  },
+  threeToOne: {
+    style: 'water',
+    text: '3:1'
+  }
+};
+
+const settingsList = {
+  classic: {
+    width: 3,
+    height: 3,
+    groundTiles: {
+      wheat: 4,
+      wood: 4,
+      lamb: 4,
+      clay: 3,
+      ore: 3,
+      desert: 1
+    },
+    waterTiles: {
+      wheat: 1,
+      wood: 1,
+      lamb: 1,
+      clay: 1,
+      ore: 1,
+      threeToOne: 4
+    },
+    totalWaterTiles: 18
+  },
+  extended: {
+    width: 3,
+    height: 4,
+    groundTiles: {
+      wheat: 6,
+      wood: 6,
+      lamb: 6,
+      clay: 5,
+      ore: 5,
+      desert: 2
+    },
+    waterTiles: {
+      wheat: 1,
+      wood: 1,
+      lamb: 2,
+      clay: 1,
+      ore: 1,
+      threeToOne: 5
+    },
+    totalWaterTiles: 22
   }
 };
 
@@ -36,24 +85,8 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
 }
 
-function drawTile(svg,x, y, size, tile) {
-  const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  svg.appendChild(polygon);
-  polygon.setAttribute("class", tile.type.style);
-  const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-  text.textContent = tile.type.text;
-  text.setAttribute("x", x);
-  text.setAttribute("y", y);
-  text.setAttribute("text-anchor", "middle");
-  svg.appendChild(text);
-
-  for (let side = 0; side < 7; side++) {
-    const angle = side * 2 * Math.PI / 6 + Math.PI / 6;
-    const point = svg.createSVGPoint();
-    point.x = x + size * Math.cos(angle);
-    point.y = y + size * Math.sin(angle);
-    polygon.points.appendItem(point);
-  }
+function createSvgElement(tagName) {
+  return document.createElementNS('http://www.w3.org/2000/svg', tagName);
 }
 
 function addTiles(deck, tileType, count) {
@@ -65,20 +98,40 @@ function addTiles(deck, tileType, count) {
   }
 }
 
-function generateMap(mapWidth, mapHeight) {
+function addPorts(deck, portType, count) {
+  for (let i = 0; i < count; i++) {
+    const tile = {
+      type: tileTypes.water,
+      portType: portType
+    };
+    deck.push(tile);
+  }
+}
+
+function generateMap(settings) {
+
+  const mapWidth = settings.width + 1;
+  const mapHeight = settings.height + 1;
   const deck = [];
   const waterDeck = [];
 
-  addTiles(waterDeck, tileTypes.water, 22);
+  const waterTiles = settings.waterTiles;
+  Object.keys(waterTiles)
+    .forEach(tileTypeName => {
+      const tilesCount = waterTiles[tileTypeName];
+      const tileType = tileTypes[tileTypeName];
+      addPorts(waterDeck, tileType, tilesCount);
+    });
 
-  addTiles(deck, tileTypes.wheat, 6);
-  addTiles(deck, tileTypes.wood, 6);
-  addTiles(deck, tileTypes.lamb, 6);
-  addTiles(deck, tileTypes.clay, 4);
-  addTiles(deck, tileTypes.ore, 6);
-  addTiles(deck, tileTypes.desert, 2);
+  addTiles(waterDeck, tileTypes.water, settings.totalWaterTiles - waterDeck.length);
 
-  console.log("Deck size " + deck.length);
+  const groundTiles = settings.groundTiles;
+  Object.keys(groundTiles)
+    .forEach(tileTypeName => {
+      const tilesCount = groundTiles[tileTypeName];
+      const tileType = tileTypes[tileTypeName];
+      addTiles(deck, tileType, tilesCount);
+    });
 
   const map = [];
   let width = mapWidth;
@@ -101,33 +154,76 @@ function generateMap(mapWidth, mapHeight) {
       width--;
     }
   }
+
+  console.log(waterDeck);
+
   return map;
 }
 
-function ready() {
-  const mapWidth = 3 + 1;
-  const mapHeight = 4 + 1;
+function addPoint(svg, x, y, size, angle, polygon) {
+  const point = svg.createSVGPoint();
+  point.x = x + size * Math.cos(angle);
+  point.y = y + size * Math.sin(angle);
+  polygon.points.appendItem(point);
+  return point;
+}
+function drawTile(svg,x, y, size, tile) {
+  const polygon = createSvgElement('polygon');
+  svg.appendChild(polygon);
+  polygon.setAttribute('class', tile.type.style);
+  const text = createSvgElement('text');
+  text.textContent = tile.portType ? tile.portType.text : tile.type.text;
+  text.setAttribute('x', x);
+  text.setAttribute('y', y);
+  text.setAttribute('text-anchor', 'middle');
+  svg.appendChild(text);
+
+  for (let side = 0; side < 7; side++) {
+    const angle = side * 2 * Math.PI / 6 + Math.PI / 6;
+    addPoint(svg, x, y, size, angle, polygon);
+  }
+
+  if (tile.portType) {
+    const sidePolygon = createSvgElement('polygon');
+    svg.appendChild(sidePolygon);
+    const side = 0;
+    const angle1 = side * 2 * Math.PI / 6 + Math.PI / 6;
+    const angle2 = angle1 + Math.PI / 3;
+    addPoint(svg, x, y, size * 0.9, angle1, sidePolygon);
+    addPoint(svg, x, y, size * 0.9, angle2, sidePolygon);
+  }
+}
+
+function drawMap(map, svg) {
   const tileSize = 40;
   const tileWidth = tileSize * Math.cos(Math.PI / 6) * 2;
   const tileHeight = tileSize * 2;
-  const svg = document.getElementById("svg");
+  const mapX = 400;
+  const mapY = tileSize;
 
-  const mapX = 400,
-    mapY = tileSize;
-
-  const map = generateMap(mapWidth, mapHeight);
-
-  for (let y = 0; y < mapHeight * 2 - 1; y++) {
+  for (let y = 0; y < map.length; y++) {
     const row = map[y];
     const rowWidth = row.length * tileWidth;
     for (let x = 0; x < row.length; x++) {
       const tile = row[x];
       const tileX = mapX - (rowWidth / 2) + x * tileWidth;
-      const tileY  = mapY + y * (tileHeight - (Math.sin(Math.PI / 6) * tileSize));
+      const tileY = mapY + y * (tileHeight - (Math.sin(Math.PI / 6) * tileSize));
       drawTile(svg, tileX, tileY, tileSize, tile);
     }
   }
 }
+function generate() {
+  const svg = document.getElementById("svg");
+  const type = document.querySelector('input[name="type"]:checked').value;
+  console.log("Type " + type);
+  const settings = settingsList[type];
+  const map = generateMap(settings);
 
+  while (svg.firstChild) {
+    svg.removeChild(svg.firstChild);
+  }
 
-document.addEventListener("DOMContentLoaded", ready);
+  drawMap(map, svg);
+}
+
+document.addEventListener("DOMContentLoaded", generate);
